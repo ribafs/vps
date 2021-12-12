1b9d379f2ec13ec40e6411e15f79e09dc49b53e4
Ribamar FS <ribafs@gmail.com>
2021-12-12 10:04:46 -0300

    Update

---
+++ 1MeuRoteiro/scripts/3ampUb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Criado/adaptado por Ribamar FS - http://ribafs.me
+# Script com tudo o necessário para a instalação do CMS Joomla 3 e do framework Laravel 8
+sudo apt update;
+
+# "Instalar pacotes básicos.";
+apt install -y apache2 libapache2-mod-php7.4 git mcrypt curl composer mc;
+a2enmod rewrite;
+
+apt install -y mariadb-server;
+
+# "Instalar PHP 7.4 e extensões.";
+apt install -y php7.4 php-cli php-imagick php-curl php-bz2 php-gd php-intl php-mysql php-zip php-apcu-bc php-apcu php-xml;
+apt install -y php7.4-xsl php7.4-bcmath php7.4-mbstring php7.4-gettext;
+apt install -y php-cli php-imagick php-curl php-bz2;
+
+wget http://spout.ussg.indiana.edu/linux/ubuntu/pool/main/m/memcached/memcached_1.6.9+dfsg-1_amd64.deb;
+dpkg -i memcached_1.6.9+dfsg-1_amd64.deb;
+apt install -y php-memcache;
+rm memcached_1.6.9+dfsg-1_amd64.deb;
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt install -y nodejs
+
+a2enmod rewrite;
+systemctl restart apache2;
+
+apt -y update;
+apt -y upgrade;
